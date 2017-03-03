051b10883cd6050b97037b53082580da48be0022
Xander <j-xander@users.noreply.github.com>
2017-03-03 10:20:30 -0500

    Updated page

--- registered.php
+++ registered.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 	<head>
-		<title>Registered at TPIDG</title>
+		<title>Registered | Touchpoint IDG | TPIDG.US</title>
 
 		<!-- META TAGS-->
 		<meta charset="utf-8">
@@ -11,40 +11,37 @@
 		<meta name="description" content="You are now registered at TPIDG">
 		<meta name="author" content="Touchpoint International Development Group, Inc.">
 		<meta name="keywords" content="TPIDG, Touchpoint, IDG, International Development Group, INC, Touchpoint International Development Group, INC, registered">
-		<meta name="google-site-verification" content="Yk6JHgiAcHxpIWzoNJyf6ErhFOtzgx-CuMcJsXHKffo" />
 
 		<!-- CSS -->
-		<link href="/css/main_styles.css" rel="stylesheet">
+		<link href="/css/main_styles.new.css" rel="stylesheet">
 
 	</head>
 
 	<body>
-		<?php include("resources/navigation.php"); ?>
+		<div id="wrapper">
+			<?php include("resources/navigation.new.php"); ?>
 
-		<div class="bigbox">
-			<div class="bigbox-inner">
-				<h2 class="pagehead">Next Step</h2>
-				
+
+			<div id="content">
+
 				<div class="para">
+					<h2>Next Step</h2>
 					<p>
 						You have been added to the registration queue.
 						<br><br>
 						You will receive an email within the next few minutes with additional information on registering.
 						<br><br>
-						If you need more immediate attention, you can call our headquarters at (910) 322-8805. Please leave voicemail as necessary.   
+						If you need more immediate attention, you can call our headquarters at (910)920-0350. Please leave voicemail as necessary.
 					</p>
-				</div>    
-			</div>    
-		</div>    
+				</div>
+			</div>
 
-		<?php include("resources/footer.php"); ?>
 
-		<!-- Core JavaScript -->
-		<script src="/js/jquery-1.12.4.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
-		<script src="/js/rollover.js"></script>
+		<?php include("resources/footer.new.php"); ?>
 
-		<?php include_once("resources/analytics.php"); ?>
+		</div>
+
+		<?php include("resources/footer_scripts.php"); ?>
 
 	</body>
 </html>
