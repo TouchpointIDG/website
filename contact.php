8be995696458a9998f5d5dc92ca171fc77e6f807
Xander <j-xander@users.noreply.github.com>
2017-03-03 10:10:53 -0500

    Updated page

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 
 
 	<head>
-		<title>Contact TPIDG</title>
+		<title>Contact Us | Touchpoint IDG | TPIDG.US</title>
 
 		<!-- META TAGS-->
 		<meta charset="utf-8">
@@ -12,66 +12,59 @@
 		<meta name="description" content="For details concerning training, employment or contracts concerning Touchpoint IDG INC, use this contact form.">
 		<meta name="author" content="Touchpoint International Development Group, Inc.">
 		<meta name="keywords" content="TPIDG, Touchpoint, IDG, International Development Group, INC, Touchpoint International Development Group, INC, contact">
-		<meta name="google-site-verification" content="Yk6JHgiAcHxpIWzoNJyf6ErhFOtzgx-CuMcJsXHKffo" />
 
 		<!-- CSS -->
-		<link href="/css/main_styles.css" rel="stylesheet">
+		<link href="/css/main_styles.new.css" rel="stylesheet">
 	</head>
 
 
 
 	<body>
-		<?php include("resources/navigation.php"); ?>
+		<div id="wrapper">
+			<?php include("resources/navigation.new.php"); ?>
 
-		<div class="bigbox">
-			<div class="bigbox-inner">
-				<h2 class="pagehead">Contact Us</h2>
-				
-				<form id="contactform" name="contact_us" id="email" method="post" action="mail/contact_email.php" autocomplete="on"  onsubmit="return valid_form( this )">
-					<div class="para">
-						
-						<h4 class="parahead">For details concerning training, employment or contracts, use this contact form.<br>For more immediate matters, call our office at <i>(910) 322-8805</i></h4>    
-						
-						<center id="contactuserinfo">
-							<input type="text" placeholder="Name" name="name" required>
+			<div id="content">
 
-							<input type="email" placeholder="Email" name="email" required>
+				<div class="para">
+					<form id="contactform" name="contact_us" id="email" method="post" action="mail/contact_email.php" autocomplete="on"  onsubmit="return valid_form( this )">
 
-							<input type="text" placeholder="Phone" name="phone" required>
-						</center>
-						
-						<div id="contacttextarea">
-							<textarea rows="4" placeholder="Your message here.." name="message" required></textarea><br>
-						</div>
+							<h4 class="parahead">For operational or training assistance please select high priority and for employment or contract assistance select medium or low.  All messages sent from this page are automatically PGP encrypted. You can also call us at (910) 920-0350.</h4>
 
-						<div id="contact_options">Priority:<br>
-							<select required name="priority">
-								<option value="Low">-- Low --</option>
-								<option value="Medium">-- Medium --</option>
-								<option value="High">-- High --</option>
-							</select>
-							<p style="color:red;text-shadow:0px 0px black, 0px 0px black, 0px 0px black, 0px 0px black;">All information submitted through this form is encrypted end-to-end using PGP.</p>
-							<!--
-							<input type="file" name="pubKey">
-							-->
-						</div>
-						
-						<center id="contactsubmit">
-							<button type="submit" value="Send">Send</button>
-						</center>
-					</div>
-				</form>
+							<a style="display:block;" href="https://tpidg.us/mail/publicKeys/info@tpidg.us.asc">info@tpidg.us Public PGP Key</a>
+
+								<input type="text" placeholder="Name" name="name" required>
+
+								<input type="email" placeholder="Email" name="email" required>
+
+								<input type="text" placeholder="Phone" name="phone" required>
+
+							<div id="contacttextarea">
+								<textarea rows="4" placeholder="Your message here.." name="message" required></textarea><br>
+							</div>
+
+							<div id="contact_options">Priority:<br>
+								<select required name="priority">
+									<option value="Low">-- Low --</option>
+									<option value="Medium">-- Medium --</option>
+									<option value="High">-- High --</option>
+								</select>
+
+								<!--
+								<input type="file" name="pubKey">
+								-->
+							</div>
+
+							<center id="contactsubmit">
+								<button type="submit" value="Send">Send</button>
+							</center>
+					</form>
+				</div>
+
 			</div>
+			<?php include("resources/footer.new.php"); ?>
 		</div>
 
-		<?php include("resources/footer.php"); ?>
-
-		<!-- Core JavaScript -->
-		<script src="/js/jquery-1.12.4.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
-		<script src="/js/rollover.js"></script>
-
-		<?php include_once("resources/analytics.php") ?>
+		<?php include_once("resources/footer_scripts.php") ?>
 
 	</body>
 </html>
