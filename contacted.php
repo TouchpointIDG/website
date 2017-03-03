8d61bcf33357ffe869990d4484f92fc1f8feadd9
Xander <j-xander@users.noreply.github.com>
2017-03-03 10:13:00 -0500

    Updated page

--- contacted.php
+++ contacted.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 	<head>
-		<title>Contact TPIDG</title>
+		<title>You Contacted Us! | Touchpoint IDG | TPIDG.US</title>
 
 		<!-- META TAGS-->
 		<meta charset="utf-8">
@@ -11,40 +11,36 @@
 		<meta name="description" content="For details concerning training, employment or contracts concerning Touchpoint IDG INC, use this contact form.">
 		<meta name="author" content="Touchpoint International Development Group, Inc.">
 		<meta name="keywords" content="TPIDG, Touchpoint, IDG, International Development Group, INC, Touchpoint International Development Group, INC, contact">
-		<meta name="google-site-verification" content="Yk6JHgiAcHxpIWzoNJyf6ErhFOtzgx-CuMcJsXHKffo" />
 
 		<!-- CSS -->
-		<link href="/css/main_styles.css" rel="stylesheet">
+		<link href="/css/main_styles.new.css" rel="stylesheet">
 
 	</head>
 
 	<body>
-		<?php include("resources/navigation.php"); ?>
+		<div id="wrapper">
+			<?php include("resources/navigation.new.php"); ?>
 
-			<div class="bigbox">
-				<div class="bigbox-inner">
-					<h2 class="pagehead">You've Contacted Us!</h2>
-					
-					<div class="para">
-						<p>
-							Your message has been delivered to the appropriate recipient. 
-							<br><br>
-							You will be contacted in a timely manner based on the priority level of your submitted request.
-							<br><br>
-							If you need more immediate attention, you can try calling our headquarters at (910) 322-8805. Please leave voicemail as necessary.   
-						</p>
-					</div>    
-				</div>    
-			</div>    
 
-		<?php include("resources/footer.php"); ?>
+			<div id="content">
+				<div class="para">
+					<h2>You've Contacted Us!</h2>
 
-		<!-- Core JavaScript -->
-		<script src="/js/jquery-1.12.4.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
-		<script src="/js/rollover.js"></script>
+					<p>
+						Your message has been delivered to the appropriate recipient.
+						<br><br>
+						You will be contacted in a timely manner based on the priority level of your submitted request.
+						<br><br>
+						If you need more immediate attention, you can try calling our headquarters at (910)920-0350. Please leave voicemail as necessary.
+					</p>
 
-		<?php include_once("resources/analytics.php") ?>
+				</div>
+			</div>
+
+			<?php include("resources/footer.new.php"); ?>
+		</div>
+
+		<?php include("resources/footer_scripts.php"); ?>
 
 	</body>
 </html>
