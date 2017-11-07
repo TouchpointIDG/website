295a29176cbfecfb3850cc3104705776f291da00
Xander <j-xander@users.noreply.github.com>
2017-11-06 22:42:28 -0500

    Add files via upload

---
+++ newsletters/downloads/NVC/deception-of-the-month-july.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+	<head>
+		<title></title>
+
+		<!-- META TAGS-->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="Touchpoint International Development Group, Inc.">
+		<meta name="keywords" content="TPIDG, Touchpoint, IDG, International Development Group, INC, Touchpoint International Development Group, INC">
+
+		<!-- CSS -->
+		<link href="/css/main_styles.new.css" rel="stylesheet">
+
+	</head>
+
+
+	<body>
+		<div id="wrapper">
+			<?php include("/var/www/html/resources/navigation.new.php"); ?>
+
+			<div id="content">
+
+					<div class="para">
+
+						<h3 class="parahead">Obama on Hillary Clinton's Email Server</h3>
+
+            <video style="width:75%; min-height: 100px; height: auto;" controls>
+              <source src="videos/obama_on_email_server.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+            </video>
+
+					</div>
+
+			</div>
+
+			<?php include("/var/www/html/resources/footer.new.php"); ?>
+
+			<?php include_once("/var/www/html/resources/footer_scripts.php") ?>
+
+		</div>
+	</body>
+</html>
